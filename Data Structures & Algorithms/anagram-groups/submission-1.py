0ed5c05c0b0574129c93f9256a11fb5eef701303
harshitaiitp-dotcom <harshitaiitp@gmail.com>
2026-07-09 03:07:43 +0530

    Bulk sync: 9 submissions

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict1 = {}
+        for s in strs:
+            key = "".join(sorted(s))
+            if key in dict1:
+                dict1[key].append(s)
+            else:
+                dict1[key] = [s] 
+        return list(dict1.values())
