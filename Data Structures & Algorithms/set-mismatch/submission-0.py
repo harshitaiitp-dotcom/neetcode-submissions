a20d21317dd6642e0a10c22a56cfec6dd0530851
Harshita <harshitatech6@gmail.com>
2026-09-13 21:54:46 +0530

    Add: set-mismatch - submission-0

---
+++ Data Structures & Algorithms/set-mismatch/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        unique_sum = sum(set(nums))
+        duplicate = actual_sum - unique_sum
+        missing = expected_sum - unique_sum
+        return [duplicate, missing]
