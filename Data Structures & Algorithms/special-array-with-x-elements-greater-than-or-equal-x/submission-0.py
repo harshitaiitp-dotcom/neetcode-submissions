be69ea1ac863fe58881623d2ee76236aa9cf7a3f
Harshita <harshitatech6@gmail.com>
2026-09-19 23:09:26 +0530

    Add: special-array-with-x-elements-greater-than-or-equal-x - submission-0

---
+++ Data Structures & Algorithms/special-array-with-x-elements-greater-than-or-equal-x/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        nums.sort(reverse=True)
+        n = len(nums)
+        for x in range(1, n + 1):
+            if nums[x - 1] >= x:
+                if x == n or nums[x] < x:
+                    return x    
+        return -1
