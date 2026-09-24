065e5a507e1524169c01302d9cae0a6578f2e286
Harshita <harshitatech6@gmail.com>
2026-09-24 22:43:25 +0530

    Add: relative-sort-array - submission-0

---
+++ Data Structures & Algorithms/relative-sort-array/submission-0.py
@@ -0,0 +1,12 @@
+from typing import List
+from collections import Counter
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        count = Counter(arr1)
+        result = []
+        for num in arr2:
+            result.extend([num] * count[num])
+            del count[num]  # Remove processed element
+        for num in sorted(count.keys()):
+            result.extend([num] * count[num])            
+        return result
